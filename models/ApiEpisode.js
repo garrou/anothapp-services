2b58048f3928342b5bf437509fdb41a9e1d43db4
garrou <garrousteadrien@gmail.com>
2023-12-10 23:08:19 +0100

    Add episode global number

--- models/ApiEpisode.js
+++ models/ApiEpisode.js
@@ -7,6 +7,7 @@ class ApiEpisode {
         this.id = episode.id;
         this.title = episode.title
         this.code = episode.code;
+        this.global = episode.global;
         this.description = episode.description;
         this.date = episode.date;
     }
