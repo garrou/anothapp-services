b142e1b22d10a1d596475a85df67de424336450c
garrou <garrousteadrien@gmail.com>
2025-03-19 23:33:00 +0100

    Add env template and update migration script

--- migrations/init.sql
+++ migrations/init.sql
@@ -1,5 +1,30 @@
 SET client_encoding = "UTF8";
 
+CREATE TABLE platforms (
+    id INTEGER,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    logo VARCHAR(255),
+    PRIMARY KEY(id)
+);
+
+INSERT INTO platforms (id, name, logo) VALUES
+(999, 'Autres', NULL),
+(25, 'ADN', 'https://pictures.betaseries.com/platforms/25.jpg'),
+(1, 'Netflix', 'https://pictures.betaseries.com/platforms/1.jpg'),
+(278, 'Canal+', 'https://pictures.betaseries.com/platforms/278.jpg'),
+(3, 'Prime Video', 'https://pictures.betaseries.com/platforms/3.jpg'),
+(2, 'OCS', 'https://pictures.betaseries.com/platforms/2.jpg'),
+(246, 'Disney+', 'https://pictures.betaseries.com/platforms/246.jpg'),
+(255, 'Apple TV+', 'https://pictures.betaseries.com/platforms/255.jpg'),
+(418, 'Max', 'https://pictures.betaseries.com/platforms/418.jpg'),
+(33, 'Paramount+', 'https://pictures.betaseries.com/platforms/33.jpg'),
+(221, 'Crunchyroll', 'https://pictures.betaseries.com/platforms/221.jpg'),
+(252, 'Rakuten Viki', 'https://pictures.betaseries.com/platforms/252.jpg'),
+(416, 'TF1+', 'https://pictures.betaseries.com/platforms/416.jpg'),
+(26, 'Arte', 'https://pictures.betaseries.com/platforms/26.jpg'),
+(17, 'M6+', 'https://pictures.betaseries.com/platforms/17.jpg'),
+(27, 'france.tv', 'https://pictures.betaseries.com/platforms/27.jpg');
+
 CREATE TABLE users (
     id VARCHAR(50),
     username VARCHAR(25) UNIQUE NOT NULL,
@@ -63,31 +88,6 @@ CREATE TABLE friends (
     FOREIGN KEY(fst_user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY(sec_user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-
-CREATE TABLE platforms (
-    id INTEGER,
-    name VARCHAR(50) UNIQUE NOT NULL,
-    logo VARCHAR(255),
-    PRIMARY KEY(id)
-);
-
-INSERT INTO platforms (id, name, logo) VALUES
-(999, 'Autres', NULL),
-(25, 'ADN', 'https://pictures.betaseries.com/platforms/25.jpg'),
-(1, 'Netflix', 'https://pictures.betaseries.com/platforms/1.jpg'),
-(278, 'Canal+', 'https://pictures.betaseries.com/platforms/278.jpg'),
-(3, 'Prime Video', 'https://pictures.betaseries.com/platforms/3.jpg'),
-(2, 'OCS', 'https://pictures.betaseries.com/platforms/2.jpg'),
-(246, 'Disney+', 'https://pictures.betaseries.com/platforms/246.jpg'),
-(255, 'Apple TV+', 'https://pictures.betaseries.com/platforms/255.jpg'),
-(418, 'Max', 'https://pictures.betaseries.com/platforms/418.jpg'),
-(33, 'Paramount+', 'https://pictures.betaseries.com/platforms/33.jpg'),
-(221, 'Crunchyroll', 'https://pictures.betaseries.com/platforms/221.jpg'),
-(252, 'Rakuten Viki', 'https://pictures.betaseries.com/platforms/252.jpg'),
-(416, 'TF1+', 'https://pictures.betaseries.com/platforms/416.jpg'),
-(26, 'Arte', 'https://pictures.betaseries.com/platforms/26.jpg'),
-(17, 'M6+', 'https://pictures.betaseries.com/platforms/17.jpg'),
-(27, 'france.tv', 'https://pictures.betaseries.com/platforms/27.jpg');
 
 CREATE TABLE users_list (
     user_id VARCHAR(50),
