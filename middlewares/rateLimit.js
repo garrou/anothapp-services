6fa8a11c3c6790e095f5e6adb8b022684d959f17
Adrien Garrouste <39885692+garrou@users.noreply.github.com>
2026-08-25 10:10:16 +0200

    Increase rate limit from 200 to 2500 requests

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -2,7 +2,7 @@ import { rateLimit, ipKeyGenerator } from 'express-rate-limit'
 
 export const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
-	limit: 200,
+	limit: 2500,
 	standardHeaders: 'draft-8',
 	legacyHeaders: false,
 	ipv6Subnet: 56,
